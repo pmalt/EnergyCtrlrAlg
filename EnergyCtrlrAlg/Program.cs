6e64a0657f5340c17e3fb84f1d2c8137d7803d1b
mpyroth <malte.pyroth@stud.htwk-leipzig.de>
2023-01-03 13:37:38 +0100

    deleted StreamWriter.cs

--- EnergyCtrlrAlg/Program.cs
+++ EnergyCtrlrAlg/Program.cs
@@ -11,15 +11,12 @@ namespace EnergyCtrlrAlg
             // todo foreach period: each FR decides about requesting charge, CP accepts or denies
             // create string for every action 
             // have string[] to collect output for now
-            // call writer (in the end) to write output to txt file (maybe earlier)
             var forecast = new List<ForecastedBlock>();
             var allFrs = new List<FlexibilityResource>();        
             var ctrlr = new SimultaneityCtrlr(forecast, allFrs);
-            var writer = new StreamWriterClass();
             // todo define output
-            // todo define file path
             string[] output = new string[] { };
-            File.WriteAllLines("path", output);
+            File.WriteAllLines("/home/malte/RiderProjects/EnergyCtrlrAlg", output);
         }
     }
 }
